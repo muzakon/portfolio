78733b4533683babf4e463e4737cde0f62d299fc
muzak <muzak@muzaks-MacBook-Pro.local>
2024-09-25 16:38:41 +0300

    Update Timestamp

--- src/views/projects/epoch-converter/Index.vue
+++ src/views/projects/epoch-converter/Index.vue
@@ -11,9 +11,10 @@
       <div class="flex items-center justify-between">
         <a-tooltip placement="bottom">
           <template #title>
-            <span>Click to copy :)</span>
+            <span>Click to copy!</span>
           </template>
           <span
+            @click="copyTimestamp"
             class="hover:text-[#00b96b] cursor-pointer text-[24px] font-semibold py-2 px-5 min-w-[250px] flex items-center justify-center rounded-[5px] hover:bg-[#131313] transition duration-200"
           >
             <span class="min-w-[190px]">{{ currentTimestamp }}</span>
@@ -84,24 +85,72 @@
         <div v-if="result" class="pt-5">
           <div
             v-if="!result.success"
-            class="bg-[#e9535314] text-rose-600 text-[13px] p-4 rounded-[3px]"
+            class="bg-[#e9535314] text-rose-600 text-[13px] p-4 rounded-[3px] border border-[#e953536b]"
           >
             {{ result.detail }}
           </div>
 
-          <div
-            v-else
-            class="bg-[#e9535314] text-green-600 text-[13px] p-4 rounded-[3px]"
-          >
-            {{ result.detail }}
+          <div v-else class="text-[14px]">
+            <div class="mb-2 text-[#a1a1a1]">
+              <span class="font-semibold text-[#bbb]">Local</span>:
+              {{ result.local }}
+            </div>
+            <div class="mb-2 text-[#a1a1a1]">
+              <span class="font-semibold text-[#bbb]">GMT</span>:
+              {{ result.gmt }}
+            </div>
+            <div class="mb-2 text-[#a1a1a1]">
+              <span class="font-semibold text-[#bbb]">Relative</span>:
+              {{ result.relative }}
+            </div>
+            <div
+              class="text-[#a1a1a1]"
+              :class="{
+                'mb-2': result.timestamp,
+              }"
+            >
+              <span class="font-semibold text-[#bbb]">ISO</span>:
+              {{ result.iso }}
+            </div>
+
+            <div class="text-[#a1a1a1]" v-if="result.timestamp">
+              <span class="font-semibold text-[#bbb]">Epoch</span>:
+              {{ result.timestamp }}
+            </div>
           </div>
         </div>
-      </div>
 
-      <!-- <div class="text-center">
-        <div>GMT: {{ timestampToDate.gmt }}</div>
-        <div>Local: {{ timestampToDate.local }}</div>
-      </div> -->
+        <div class="pt-[48px]">
+          <h1 class="font-semibold text-[16px] mb-2">
+            What is Unix/Epoch Timestamp ?
+          </h1>
+          <p class="text-[13px] text-[#bbb]">
+            The Unix Epoch Timestamp is simply a representation of time in
+            seconds or milliseconds. It measures the time elapsed since January
+            1st, 1970, at UTC (or GMT) to any particular moment, expressed in
+            seconds (or milliseconds), and is referred to as the timestamp of
+            that instance. The timestamp exclusively represents the time in UTC
+            . Some systems present the time as a 32-bit integer, which may lead
+            to an issue on January 19, 2038 (also known as Y2038). The efficient
+            tool on this page will assist you in converting timestamps from
+            seconds (10-digit), milliseconds (13-digit) and microseconds
+            (16-digit) into human-readable DateTime.
+          </p>
+
+          <h1 class="font-semibold text-[16px] mb-2 pt-[20px]">
+            About ISO 8601 date and time
+          </h1>
+          <p class="text-[13px] text-[#bbb]">
+            ISO 8601 is a date and time notation format defined by ISO as an
+            international standard. The date and time are connected by "T" and
+            written. The time zone is expressed as "+09:00" as the difference
+            time from UTC, and in the case of UTC, it is expressed as "Z".
+            Separate seconds and milliseconds with a comma (,) or dot (.).
+            DenCode omits milliseconds when milliseconds are 000. ISO 8601 comes
+            in several formats.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -111,8 +160,12 @@ import { onMounted, ref, type Ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { useClipboard } from "@vueuse/core";
+import { notification } from "ant-design-vue";
 
 dayjs.extend(utc);
+dayjs.extend(relativeTime);
 
 import { computed } from "vue";
 
@@ -137,6 +190,9 @@ const updateTimestamp: Ref<number | null> = ref(null);
 const userTimestamp: Ref<string> = ref("");
 const userDate = ref(null);
 const result = ref(null);
+const { text, copy, copied, isSupported } = useClipboard({
+  source: currentTimestamp,
+});
 
 function getTimestamp(): number {
   const timestamp = +Date.now();
@@ -155,6 +211,9 @@ onMounted(() => {
   updateTimestamp.value = setInterval(() => {
     currentTimestamp.value = getTimestamp();
   }, 1000);
+
+  userTimestamp.value = getTimestamp().toString();
+  convert();
 });
 
 watch(timestampType, (newValue) => {
@@ -171,22 +230,53 @@ watch(timestampType, (newValue) => {
   }
 });
 
+watch(userTimestamp, (newValue) => {
+  if (newValue) {
+    if (userDate.value) userDate.value = null;
+  }
+});
+
+watch(userDate, (newValue) => {
+  if (newValue) {
+    if (userTimestamp.value) userTimestamp.value = "";
+  }
+});
+
 function convert() {
   if (userTimestamp.value) {
     try {
-      let date;
-
       // Check if timestamp is in seconds or milliseconds
       if (userTimestamp.value.length === 10) {
         // 10 digits = seconds
-        date = dayjs
-          .unix(parseInt(userTimestamp.value))
-          .format("dddd, MMMM D, YYYY HH:mm:ss");
+        result.value = {
+          local: dayjs
+            .unix(parseInt(userTimestamp.value))
+            .format("dddd, MMMM D, YYYY HH:mm:ss"),
+          gmt: dayjs
+            .unix(parseInt(userTimestamp.value))
+            .utc(false)
+            .format("dddd, MMMM D, YYYY HH:mm:ss"),
+
+          relative: dayjs.unix(parseInt(userTimestamp.value)).fromNow(),
+
+          iso: dayjs.unix(parseInt(userTimestamp.value)).toISOString(),
+          success: true,
+        };
       } else if (userTimestamp.value.length === 13) {
         // 13 digits = milliseconds
-        date = dayjs(parseInt(userTimestamp.value)).format(
-          "dddd, MMMM D, YYYY HH:mm:ss"
-        );
+        result.value = {
+          local: dayjs(parseInt(userTimestamp.value)).format(
+            "dddd, MMMM D, YYYY HH:mm:ss"
+          ),
+          gmt: dayjs(parseInt(userTimestamp.value))
+            .utc(false)
+            .format("dddd, MMMM D, YYYY HH:mm:ss"),
+
+          relative: dayjs(parseInt(userTimestamp.value)).fromNow(),
+
+          iso: dayjs(parseInt(userTimestamp.value)).toISOString(),
+          success: true,
+        };
       } else {
         throw new Error("Invalid timestamp format.");
       }
@@ -196,6 +286,37 @@ function convert() {
         detail: error.message,
       };
     }
+  } else if (userDate.value) {
+    result.value = {
+      local: dayjs(userDate.value, "YYYY-MM-DD HH:mm:ss").format(
+        "dddd, MMMM D, YYYY HH:mm:ss"
+      ),
+      gmt: dayjs(userDate.value, "YYYY-MM-DD HH:mm:ss")
+        .utc(false)
+        .format("dddd, MMMM D, YYYY HH:mm:ss"),
+
+      relative: dayjs(userDate.value, "YYYY-MM-DD HH:mm:ss").fromNow(),
+
+      iso: dayjs(userDate.value, "YYYY-MM-DD HH:mm:ss").toISOString(),
+      success: true,
+
+      timestamp: dayjs(userDate.value, "YYYY-MM-DD HH:mm:ss").unix(),
+    };
+  }
+}
+
+function copyTimestamp() {
+  try {
+    copy();
+    notification["success"]({
+      message: "Copied!",
+      description: "The timestamp has been copied to your clipboard.",
+    });
+  } catch (error) {
+    notification["error"]({
+      message: "Error",
+      description: error.message,
+    });
   }
 }
 </script>
